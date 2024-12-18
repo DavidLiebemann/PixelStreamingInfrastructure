714c6a5383ec951fa2fed60fbacdb8d0f922334c
David Liebemann <david@scico.dev>
2024-12-18 17:36:19 +0100

    Started https process

--- Custom/start_signalling_servers.sh
+++ Custom/start_signalling_servers.sh
@@ -5,7 +5,8 @@ IMAGE_NAME="pixelstreaming-signallingwebserver5.3"
 
 # Start Ports
 MATCHMAKER_PORT=9999
-BASE_SIGNAL_PORT=81
+BASE_HTTP_PORT=81
+BASE_HTTPS_PORT=8443
 BASE_STREAMER_PORT=8888
 BASE_SFU_PORT=9000
 
@@ -19,11 +20,23 @@ NUM_SERVERS=${1:-$DEFAULT_NUM_SERVERS}
 # Determine Server-IP
 SERVER_IP=$(hostname -I | awk '{print $1}')
 
+# Name of stop and remove script
+STOP_SCRIPT="stop_and_remove_containers_by_image.sh"
+
+# Prüfen, ob das andere Skript existiert
+if [ -f "$STOP_SCRIPT" ]; then
+    echo "Calling stop and remove script..."
+    bash "$STOP_SCRIPT"
+else
+    echo "Stop script $STOP_SCRIPT not found. Skipping stopping and removing already running signalling servers."
+fi
+
+
 echo "Starting $NUM_SERVERS Signalling Servers..."
 
 # starting given number of signalling servers
 for (( i=0; i<$NUM_SERVERS; i++ )); do
-    SIGNAL_PORT=$(( BASE_SIGNAL_PORT + i ))      # Signal-Port: 81, 82, 83, ...
+    SIGNAL_PORT=$(( BASE_HTTP_PORT + i ))      # Signal-Port: 81, 82, 83, ...
     STREAMER_PORT=$(( BASE_STREAMER_PORT + i ))  # Streamer-Port: 8888, 8889, ...
     SFU_PORT=$(( BASE_SFU_PORT + i ))            # SFU-Port: 9000, 9001, ...
     CONTAINER_NAME="signalling_server_$(( i + 1 ))"
